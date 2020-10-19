cc908deb0dd41afc53ff62826749a5ff25980459
Maximilian Stock <mxmlnstock@googlemail.com>
2020-10-19 19:42:58 +0200

    feat: By default persist via CSVLog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod client;
 mod indexer;
 mod parser;
 mod persistence;
@@ -15,27 +16,15 @@ use std::env;
 #[macro_use]
 extern crate lazy_static;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("No database url set");
-    let persistence = persistence::PgDb::new(&database_url);
+    // let database_url = env::var("DATABASE_URL").expect("No database url set");
+    // let persistence = persistence::PgDb::new(&database_url);
+    let persistence = persistence::CSVLog::new("log.csv");
 
-    let change_id = match persistence.get_last_read_change_id().ok() {
-        Some(id) => {
-            println!("Proceed indexing after {:?}", id);
-            id
-        }
-        None => {
-            let default_change_id =
-                env::var("DEFAULT_CHANGE_ID").expect("No default change id set");
-            eprintln!(
-                "Could not find last change id -> Use default {}",
-                default_change_id
-            );
-            default_change_id
-        }
-    };
-
+    let latest_change_id = client::fetch_latest_change_id()?;
     let mut indexer = Indexer::new(&persistence);
-    indexer.start(change_id);
+    indexer.start(latest_change_id);
+
+    Ok(())
 }
