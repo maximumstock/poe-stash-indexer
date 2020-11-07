2bd94fa368f1188f40a26f3cbd20223375a575b7
Maximilian Stock <mxmlnstock@googlemail.com>
2020-11-07 21:03:24 +0100

    feat: Go back to using PostgreSQL persistence by default

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -15,14 +15,9 @@ extern crate lazy_static;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     pretty_env_logger::init();
-    // // let database_url = env::var("DATABASE_URL").expect("No database url set");
-    // // let persistence = persistence::PgDb::new(&database_url);
-    // let persistence = persistence::CSVLog::new("log.csv");
 
-    // let latest_change_id = client::RiverClient::fetch_latest_change_id()?;
-    // let client = client::RiverClient {};
-    // let mut indexer = Indexer::new(&persistence, &client);
-    // indexer.start(latest_change_id);
+    let database_url = std::env::var("DATABASE_URL").expect("No database url set");
+    let persistence = persistence::PgDb::new(&database_url);
 
     let indexer = Indexer::new();
     let rx = indexer.start_with_latest()?;
