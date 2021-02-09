c0b6e9fd13890ea976d2ab0cddcb6319b36a99dc
Maximilian Stock <mxmlnstock@googlemail.com>
2021-02-09 21:54:32 +0100

    ops: Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustStableChannel = nixpkgs.latest.rustChannels.stable.rust.override {
+    extensions = [
+      "rust-src"
+      "rls-preview"
+      "clippy-preview"
+      "rustfmt-preview"
+    ];
+  };
+in
+nixpkgs.mkShell {
+  name = "rust-dev-env";
+  buildInputs = with nixpkgs; [
+    rustStableChannel
+    rust-analyzer
+    postgresql
+  ];
+}
