65237eab412cad473e11d86c1c4b8b9718bf550a
Maximilian Stock <mxmlnstock@googlemail.com>
2021-03-02 19:57:00 +0100

    fix: indexer config file path

--- indexer/src/config.rs
+++ indexer/src/config.rs
@@ -1,7 +1,7 @@
 use config::{Config, ConfigError, File};
 use serde::Deserialize;
 
-const CONFIG_FILE_PATH: &str = "./indexer/config/config.toml";
+const CONFIG_FILE_PATH: &str = "./config/config.toml";
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Configuration {
