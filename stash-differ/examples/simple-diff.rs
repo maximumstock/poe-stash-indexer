41600a7e00285ae3638acffe5cfaee475241924d
Maximilian Stock <mxmlnstock@googlemail.com>
2021-03-03 01:30:41 +0100

    feat: Do diffing
    - Add stack_size & note diff events
    - Add metadata to diff events to indicate what changed
    - Add basic diff event statistics at the end of simple-diff
    - Update main documentation

--- stash-differ/examples/simple-diff.rs
+++ stash-differ/examples/simple-diff.rs
@@ -1,13 +1,27 @@
-use stash_differ::{SampleImporter, StashDiffer};
+use stash_differ::{DiffEvent, DiffStats, SampleImporter, StashDiffer};
 
 fn main() {
     let stash_records = SampleImporter::from_file("./sample_raw.csv");
+
+    let mut stats = DiffStats::default();
 
     stash_records
         .iter()
         .zip(stash_records.iter().skip(1))
         .for_each(|(before, after)| {
             let events = StashDiffer::diff(&before, &after);
-            dbg!(events);
+
+            if !events.is_empty() {
+                events.iter().for_each(|e| match e {
+                    DiffEvent::ItemAdded(..) => stats.added += 1,
+                    DiffEvent::ItemRemoved(..) => stats.removed += 1,
+                    DiffEvent::ItemNoteChanged(..) => stats.note += 1,
+                    DiffEvent::ItemStackSizeChanged(..) => stats.stack_size += 1,
+                });
+
+                dbg!(events);
+            }
         });
+
+    dbg!(&stats);
 }
