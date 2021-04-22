107043352d68e111f6adc7e0dc18898a15a5dae8
Maximilian Stock <mxmlnstock@googlemail.com>
2021-04-22 23:35:14 +0200

    feat: Add database stash diffing

--- stash-differ/src/lib.rs
+++ stash-differ/src/lib.rs
@@ -1,75 +1,164 @@
-use csv::ReaderBuilder;
 use serde::Deserialize;
-use std::collections::HashMap;
+use sqlx::{postgres::PgRow, Row};
+use std::collections::{HashMap, HashSet};
 
 pub struct StashDiffer;
 
 impl StashDiffer {
-    pub fn diff(before: &StashRecord, after: &StashRecord) -> Vec<DiffEvent> {
+    pub fn diff(before: &Stash, after: &Stash) -> Vec<DiffEvent> {
         let mut events = vec![];
 
+        let before_item_ids = before.content.keys().collect::<HashSet<_>>();
+        let after_item_ids = after.content.keys().collect::<HashSet<_>>();
+
+        let removed_item_ids = before_item_ids.difference(&after_item_ids);
+        let added_item_ids = after_item_ids.difference(&before_item_ids);
+        let changed_item_ids = after_item_ids.intersection(&before_item_ids);
+
         // Check for removed items
-        for before_item in before.items.iter() {
-            if after
-                .items
-                .iter()
-                .find(|i| before_item.id.eq(&i.id))
-                .is_none()
-            {
-                events.push(DiffEvent::ItemRemoved(Diff {
-                    before: (),
-                    after: (),
-                    id: before_item.id.clone(),
-                    name: before_item.type_line.clone(),
-                }));
-                continue;
-            }
+        for &item_id in removed_item_ids {
+            let item = before.content.get(item_id).unwrap();
+            events.push(DiffEvent::ItemRemoved(Diff {
+                before: (),
+                after: (),
+                id: item.id.clone(),
+                name: item.type_line.clone(),
+            }));
         }
 
         // Check for added items
-        for after_item in after.items.iter() {
-            if before
-                .items
-                .iter()
-                .find(|i| after_item.id.eq(&i.id))
-                .is_none()
-            {
-                events.push(DiffEvent::ItemAdded(Diff {
-                    before: (),
-                    after: (),
-                    id: after_item.id.clone(),
-                    name: after_item.type_line.clone(),
-                }));
-                continue;
-            }
+        for &item_id in added_item_ids {
+            let item = after.content.get(item_id).unwrap();
+            events.push(DiffEvent::ItemAdded(Diff {
+                before: (),
+                after: (),
+                id: item.id.clone(),
+                name: item.type_line.clone(),
+            }));
         }
 
         // Check for changed items
-        for after_item in after.items.iter() {
-            if let Some(before_item) = before.items.iter().find(|i| after_item.id.eq(&i.id)) {
-                // Check for changed notes
-                if before_item.note.ne(&after_item.note) {
-                    events.push(DiffEvent::ItemNoteChanged(Diff {
-                        id: after_item.id.clone(),
-                        name: after_item.type_line.clone(),
-                        before: before_item.note.clone(),
-                        after: after_item.note.clone(),
-                    }));
-                }
+        for &item_id in changed_item_ids {
+            let before_item = before.content.get(item_id).unwrap();
+            let after_item = after.content.get(item_id).unwrap();
 
-                // Check for changed stack_sizes
-                if before_item.stack_size.ne(&after_item.stack_size) {
-                    events.push(DiffEvent::ItemStackSizeChanged(Diff {
-                        id: after_item.id.clone(),
-                        name: after_item.type_line.clone(),
-                        before: before_item.stack_size,
-                        after: after_item.stack_size,
-                    }));
-                }
+            // Check for changed notes
+            if before_item.note.ne(&after_item.note) {
+                events.push(DiffEvent::ItemNoteChanged(Diff {
+                    id: after_item.id.clone(),
+                    name: after_item.type_line.clone(),
+                    before: before_item.note.clone(),
+                    after: after_item.note.clone(),
+                }));
+            }
+
+            // Check for changed stack_sizes
+            if before_item.stack_size.ne(&after_item.stack_size) {
+                events.push(DiffEvent::ItemStackSizeChanged(Diff {
+                    id: after_item.id.clone(),
+                    name: after_item.type_line.clone(),
+                    before: before_item.stack_size,
+                    after: after_item.stack_size,
+                }));
             }
         }
 
         events
+    }
+}
+#[derive(Debug, Clone)]
+pub struct StashRecord {
+    pub change_id: String,
+    pub next_change_id: String,
+    pub stash_id: String,
+    pub items: Vec<Item>,
+    pub account_name: Option<String>,
+    pub league: Option<String>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Item {
+    pub id: String,
+    pub type_line: String,
+    pub note: Option<String>,
+    pub stack_size: Option<u32>,
+}
+
+impl<'r> sqlx::FromRow<'r, PgRow> for StashRecord {
+    fn from_row(row: &'r PgRow) -> Result<Self, sqlx::Error> {
+        Ok(StashRecord {
+            change_id: row.try_get("change_id")?,
+            next_change_id: row.try_get("next_change_id")?,
+            stash_id: row.try_get("stash_id")?,
+            account_name: row.try_get::<Option<String>, &str>("account_name")?,
+            league: row.try_get::<Option<String>, &str>("league")?,
+            items: row
+                .try_get("items")
+                .map(serde_json::from_value::<Vec<Item>>)
+                .expect("JSON deserialization failed")
+                .unwrap(),
+        })
+    }
+}
+
+type AccountName = String;
+type ItemId = String;
+/// The cumulative state of all stashes for a given `account_name`
+pub struct Stash {
+    account_name: AccountName,
+    content: HashMap<ItemId, Item>,
+}
+
+impl From<&[StashRecord]> for Stash {
+    fn from(stash_records: &[StashRecord]) -> Self {
+        Self {
+            account_name: stash_records
+                .first()
+                .unwrap()
+                .account_name
+                .as_ref()
+                .unwrap()
+                .clone(),
+            content: stash_records
+                .iter()
+                .flat_map(|sr| &sr.items)
+                .cloned()
+                .map(|i| (i.id.clone(), i))
+                .collect(),
+        }
+    }
+}
+
+pub struct LeagueStore {
+    pub inner: HashMap<AccountName, Stash>,
+}
+
+impl Default for LeagueStore {
+    fn default() -> Self {
+        Self {
+            inner: HashMap::default(),
+        }
+    }
+}
+
+impl LeagueStore {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn ingest_account(
+        &mut self,
+        account_name: &str,
+        stash_records: &[StashRecord],
+    ) -> Option<Vec<DiffEvent>> {
+        let current: Stash = stash_records.into();
+        if let Some(previous) = self.inner.get(account_name) {
+            // we can diff
+            Some(StashDiffer::diff(&previous, &current))
+        } else {
+            self.inner.insert(account_name.into(), current);
+            None
+        }
     }
 }
 
@@ -87,51 +176,6 @@ pub struct Diff<T> {
     name: String,
     before: T,
     after: T,
-}
-
-pub struct SampleImporter;
-
-impl SampleImporter {
-    pub fn from_file(file_path: &str) -> Vec<StashRecord> {
-        let mut reader = ReaderBuilder::new()
-            .from_path(file_path)
-            .expect("File not found");
-
-        reader
-            .deserialize::<HashMap<String, String>>()
-            .into_iter()
-            .map(|record| {
-                let record = record.unwrap();
-                StashRecord {
-                    stash_id: record.get("stash_id").unwrap().clone(),
-                    stash_type: record.get("stash_type").unwrap().clone(),
-                    account_name: record.get("account_name").unwrap().clone(),
-                    stash_name: record.get("stash_name").unwrap().clone(),
-                    league: record.get("league").unwrap().clone(),
-                    items: serde_json::from_str::<Vec<Item>>(&record.get("items").unwrap())
-                        .unwrap(),
-                }
-            })
-            .collect()
-    }
-}
-
-#[derive(Debug, Deserialize)]
-pub struct StashRecord {
-    stash_id: String,
-    stash_type: String,
-    items: Vec<Item>,
-    account_name: String,
-    stash_name: String,
-    league: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Item {
-    id: String,
-    type_line: String,
-    note: Option<String>,
-    stack_size: Option<u32>,
 }
 
 #[derive(Debug)]
