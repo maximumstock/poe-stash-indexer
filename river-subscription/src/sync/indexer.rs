8266e7111426ef9dfe45f07a3d96e5b98ba0b009
Maximilian Stock <mxmlnstock@googlemail.com>
2021-08-24 21:57:12 +0200

    refactor: split up sync::indexer into modules

--- river-subscription/src/sync/indexer.rs
+++ river-subscription/src/sync/indexer.rs
@@ -1,21 +1,21 @@
-use flate2::bufread::GzDecoder;
 use std::{
-    collections::VecDeque,
-    io::Write,
-    io::{BufReader, Read},
-    str::FromStr,
-    string::FromUtf8Error,
-    sync::mpsc::{channel, Receiver, Sender},
+    io::Read,
+    sync::mpsc::{channel, Receiver},
     sync::Arc,
     sync::Mutex,
 };
-use ureq::Error;
 
 use crate::common::{ChangeId, StashTabResponse};
 use crate::sync::poe_ninja_client::PoeNinjaClient;
 
+use super::{
+    fetcher::start_fetcher,
+    state::{SharedState, State},
+    worker::start_worker,
+};
+
 pub struct Indexer {
-    shared_state: SharedState,
+    pub(crate) shared_state: SharedState,
 }
 
 impl Indexer {
@@ -37,38 +37,10 @@ impl Default for Indexer {
     }
 }
 
-type SharedState = Arc<Mutex<State>>;
-
-struct State {
-    worker_queue: WorkerQueue,
-    fetcher_queue: FetcherQueue,
-    should_stop: bool,
-}
-
-impl State {
-    fn stop(&mut self) {
-        self.should_stop = true;
-    }
-}
-
-impl Default for State {
-    fn default() -> Self {
-        Self {
-            worker_queue: VecDeque::new(),
-            fetcher_queue: VecDeque::new(),
-            should_stop: false,
-        }
-    }
-}
-
-type ChangeIdRequest = (ChangeId, usize);
-type FetcherQueue = VecDeque<ChangeIdRequest>;
-type WorkerQueue = VecDeque<WorkerTask>;
-
-struct WorkerTask {
-    fetch_partial: [u8; 80],
-    change_id: ChangeId,
-    reader: Box<dyn Read + Send>,
+pub(crate) struct WorkerTask {
+    pub(crate) fetch_partial: [u8; 80],
+    pub(crate) change_id: ChangeId,
+    pub(crate) reader: Box<dyn Read + Send>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -137,180 +109,13 @@ impl Indexer {
     }
 }
 
-fn start_fetcher(shared_state: SharedState) -> std::thread::JoinHandle<()> {
-    std::thread::spawn(move || {
-        // Break down rate-limit into quantum of 1, so we never do any bursts,
-        // like we would with for example 2 requests per second.
-        let mut ratelimit = ratelimit::Builder::new()
-            .capacity(1)
-            .quantum(1)
-            .interval(std::time::Duration::from_millis(500))
-            .build();
-
-        loop {
-            if shared_state.lock().unwrap().should_stop {
-                break;
-            }
-
-            ratelimit.wait();
-
-            let change_id_request = shared_state
-                .lock()
-                .unwrap()
-                .fetcher_queue
-                .pop_front()
-                .unwrap();
-
-            let (change_id, _) = change_id_request.clone();
-
-            let start = std::time::Instant::now();
-            let url = format!(
-                "http://www.pathofexile.com/api/public-stash-tabs?id={}",
-                change_id
-            );
-
-            log::debug!("Requesting {}", change_id);
-            let request = ureq::request("GET", &url)
-                .set("Accept-Encoding", "gzip")
-                .set("Accept", "application/json");
-            let response = request.call();
-
-            if let Err(Error::Status(status, response)) = response {
-                log::error!("fetcher: HTTP error {}", status);
-                log::error!("fetcher: HTTP response: {:?}", response);
-
-                match reschedule(shared_state.clone(), change_id_request) {
-                    Ok(_) => continue,
-                    Err(_) => break,
-                }
-            }
-
-            let reader = response.unwrap().into_reader();
-            let mut decoder = GzDecoder::new(BufReader::new(reader));
-            let mut next_id_buffer = [0; 80];
-
-            match decoder.read_exact(&mut next_id_buffer) {
-                Ok(_) => {}
-                Err(err) if err.kind() == std::io::ErrorKind::UnexpectedEof => {
-                    log::error!("UnexpectedEof: {:?}", next_id_buffer);
-                    shared_state.lock().unwrap().stop();
-                    continue;
-                }
-                Err(err) => {
-                    log::error!("fetcher: gzip decoding failed: {}", err);
-
-                    match reschedule(shared_state.clone(), change_id_request.clone()) {
-                        Ok(_) => continue,
-                        Err(_) => break,
-                    }
-                }
-            }
-
-            let next_id = parse_change_id_from_bytes(&next_id_buffer)
-                .expect("Preemptive deserialization of next change_id failed");
-
-            log::debug!(
-                "Took {}ms to read next id: {}",
-                start.elapsed().as_millis(),
-                next_id
-            );
-
-            let next_change_id = ChangeId::from_str(&next_id).expect("Invalid change_id provided");
-
-            if next_change_id.eq(&change_id) {
-                ratelimit.wait_for(2);
-            }
-
-            let next_worker_task = WorkerTask {
-                reader: Box::new(decoder),
-                fetch_partial: next_id_buffer,
-                change_id,
-            };
-
-            let mut lock = shared_state.lock().unwrap();
-            lock.worker_queue.push_back(next_worker_task);
-            lock.fetcher_queue.push_back((next_change_id, 0));
-        }
-
-        shared_state.lock().unwrap().stop();
-    })
-}
-
-fn reschedule(shared_state: SharedState, request: ChangeIdRequest) -> Result<(), ()> {
-    if request.1 > 2 {
-        log::error!("Retried too many times...shutting down");
-        return Err(());
-    }
-
-    let new_request = (request.0, request.1 + 1);
-    log::info!(
-        "Rescheduling {} (Retried {} times)",
-        new_request.0,
-        request.1
-    );
-    shared_state
-        .lock()
-        .unwrap()
-        .fetcher_queue
-        .push_back(new_request);
-
-    Ok(())
-}
-
-fn start_worker(
-    shared_state: SharedState,
-    tx: Sender<IndexerMessage>,
-) -> std::thread::JoinHandle<()> {
-    std::thread::spawn(move || loop {
-        let mut lock = shared_state.lock().unwrap();
-
-        if lock.should_stop {
-            tx.send(IndexerMessage::Stop).unwrap();
-            return;
-        }
-
-        if let Some(mut task) = lock.worker_queue.pop_front() {
-            let start = std::time::Instant::now();
-            let mut buffer = Vec::new();
-            buffer.write_all(&task.fetch_partial).unwrap();
-            task.reader.read_to_end(&mut buffer).unwrap();
-            log::debug!("Took {}ms to read body", start.elapsed().as_millis());
-
-            let start = std::time::Instant::now();
-            let deserialized = serde_json::from_slice::<StashTabResponse>(&buffer)
-                .expect("Deserialization of body failed");
-            log::debug!("Took {}ms to deserialize body", start.elapsed().as_millis());
-
-            let msg = IndexerMessage::Tick {
-                payload: deserialized,
-                change_id: task.change_id,
-                created_at: std::time::SystemTime::now(),
-            };
-
-            tx.send(msg).expect("Sending IndexerMessage::Tick failed");
-        } else {
-            drop(lock);
-            log::debug!("Worker is waiting due to no work");
-            std::thread::sleep(std::time::Duration::from_millis(500));
-        }
-    })
-}
-
-fn parse_change_id_from_bytes(bytes: &[u8]) -> Result<String, FromUtf8Error> {
-    String::from_utf8(
-        bytes
-            .split(|b| (*b as char).eq(&'"'))
-            .nth(3)
-            .unwrap()
-            .to_vec(),
-    )
-}
-
 #[cfg(test)]
 mod test {
     use std::{sync::mpsc::RecvTimeoutError, time::Duration};
 
-    use super::{parse_change_id_from_bytes, Indexer, IndexerMessage};
+    use crate::sync::fetcher::parse_change_id_from_bytes;
+
+    use super::{Indexer, IndexerMessage};
 
     #[test]
     fn test_parse_change_id_from_bytes() {
--- river-subscription/src/sync/mod.rs
+++ river-subscription/src/sync/mod.rs
@@ -1,4 +1,8 @@
+mod fetcher;
 mod indexer;
-pub(crate) mod poe_ninja_client;
+mod poe_ninja_client;
+mod scheduler;
+mod state;
+mod worker;
 
 pub use indexer::{Indexer, IndexerMessage};
