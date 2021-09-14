229e3e6526ee1891b65813b8a4be825ae23b5be1
Maximilian Stock <mxmlnstock@googlemail.com>
2021-09-14 03:52:45 +0200

    fix: use TimescaleDB to improve scaling

--- indexer/migrations/2021-06-20-094806_chunk_counter/down.sql
+++ indexer/migrations/2021-06-20-094806_chunk_counter/down.sql
@@ -1,3 +1,4 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE stash_records DROP chunk_id;
-DROP INDEX stash_records_chunk_id_idx;
+
+DROP INDEX IF EXISTS stash_records_chunk_id_idx;
--- indexer/migrations/2021-06-20-094806_chunk_counter/up.sql
+++ indexer/migrations/2021-06-20-094806_chunk_counter/up.sql
@@ -1,4 +1,4 @@
 -- Your SQL goes here
-ALTER TABLE stash_records
-ADD chunk_id BIGINT NOT NULL DEFAULT 0;
+ALTER TABLE stash_records ADD chunk_id BIGINT DEFAULT NULL;
+
 CREATE INDEX stash_records_chunk_id_idx on stash_records (chunk_id);
