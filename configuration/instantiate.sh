98d49b6000b676aa67bd8b86a506dd1a135fc7a8
Maximilian Stock <mxmlnstock@googlemail.com>
2021-12-30 16:45:28 +0100

    chore: templated configuration files

---
+++ configuration/instantiate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+cp -r templates/* instantiated/
+
+# todo generalize
+set -o allexport; source environments/.env.development; set +o allexport
+
+for f in $(find instantiated -type f); do
+  SUBS=$(cat $f | envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')")
+  echo "$SUBS" > $f
+  echo "Substituted $f"
+done
