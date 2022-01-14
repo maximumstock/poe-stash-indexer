ea107c55378954a60f88572219410cf7081ecadc
Maximilian Stock <mxmlnstock@googlemail.com>
2022-01-15 00:40:19 +0100

    chore: add encryped production config

--- configuration/instantiate.sh
+++ configuration/instantiate.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
 set -e
-
 cp -r templates instantiated
+set -o allexport;
 
-# todo generalize
-set -o allexport; source environments/.env.development; set +o allexport
+case $ENV in
+  "production")
+    echo "$AGE_KEY" > key.txt
+    age --decrypt -i key.txt -o environments/.env.production environments/.env.production.enc
+    rm key.txt
+    source environments/.env.production;
+    ;;
+  *)
+    source environments/.env.development;
+    ;;
+esac
+
+set +o allexport
 
 for f in $(find instantiated -type f); do
   SUBS=$(cat $f | envsubst "$(env | cut -d= -f1 | sed -e 's/^/$/')")
