3f2e25d3e518b52273a94d0d1d14d2a6d69ccacc
Maximilian Stock <mxmlnstock@googlemail.com>
2022-06-04 23:28:52 +0200

    feat: add stash-api sync & async features

--- stash-api/src/sync/mod.rs
+++ stash-api/src/sync/mod.rs
@@ -1,7 +1,13 @@
+#[cfg(feature = "sync")]
 mod fetcher;
+#[cfg(feature = "sync")]
 mod indexer;
+#[cfg(feature = "sync")]
 mod poe_ninja_client;
+#[cfg(feature = "sync")]
 mod scheduler;
+#[cfg(feature = "sync")]
 mod worker;
 
+#[cfg(feature = "sync")]
 pub use indexer::{Indexer, IndexerMessage};
