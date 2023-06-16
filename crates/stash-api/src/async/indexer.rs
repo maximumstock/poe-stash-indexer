9f7a6742d6d91b09833741fd0cebdf97e42b8082
Maximilian Stock <mxmlnstock@googlemail.com>
2023-06-17 00:43:37 +0200

    fix: endless restarts due to seemingly empty stream on prod

--- crates/stash-api/src/async/indexer.rs
+++ crates/stash-api/src/async/indexer.rs
@@ -1,10 +1,10 @@
-use std::{collections::VecDeque, sync::Arc, time::Duration};
+use std::{sync::Arc, time::Duration};
 
 use bytes::BytesMut;
-use futures::lock::Mutex;
+use reqwest::StatusCode;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio::sync::RwLock;
-use tracing::{debug, error, error_span, info, trace, trace_span};
+use tracing::{debug, error, error_span, info, trace_span};
 
 use crate::common::parse::parse_change_id_from_bytes;
 use crate::common::poe_api::{get_oauth_token, user_agent, OAuthResponse};
@@ -63,118 +63,124 @@ impl Indexer {
             .expect("Fetch OAuth credentials");
         config.credentials = Some(credentials);
 
-        let jobs = Arc::new(Mutex::new(VecDeque::new()));
         let (tx, rx) = channel(42);
 
-        schedule_job(jobs, tx, change_id, Arc::new(RwLock::new(config)));
+        schedule_job(tx, change_id, Arc::new(RwLock::new(config)));
         rx
     }
 }
 
-fn schedule_job(
-    jobs: Arc<Mutex<VecDeque<ChangeId>>>,
-    tx: Sender<IndexerMessage>,
-    next_change_id: ChangeId,
-    config: Arc<RwLock<Config>>,
-) {
-    tokio::spawn(async move {
-        jobs.lock().await.push_back(next_change_id);
-        process(jobs, tx.clone(), config).await
-    });
+fn schedule_job(tx: Sender<IndexerMessage>, next_change_id: ChangeId, config: Arc<RwLock<Config>>) {
+    tokio::spawn(async move { process(next_change_id, tx, config).await });
 }
 
-#[tracing::instrument(skip(jobs, tx))]
+#[tracing::instrument(skip(tx))]
 async fn process(
-    jobs: Arc<Mutex<VecDeque<ChangeId>>>,
+    change_id: ChangeId,
     tx: Sender<IndexerMessage>,
     config: Arc<RwLock<Config>>,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // TODO: check if stopping
 
-    let next = jobs.lock().await.pop_back();
+    let url = format!(
+        "https://api.pathofexile.com/public-stash-tabs?id={}",
+        &change_id
+    );
+    debug!("Requesting {}", url);
 
-    if let Some(change_id) = next {
-        let url = format!(
-            "https://api.pathofexile.com/public-stash-tabs?id={}",
-            &change_id
-        );
-        trace!(change_id = ?change_id, url = ?url);
-        debug!("Requesting {}", url);
-
-        // TODO: static client somewhere
-        let client = reqwest::ClientBuilder::new().build()?;
-        let response = client
-            .get(url)
-            .header("Accept", "application/json")
-            .header("User-Agent", user_agent(&config.read().await.client_id))
-            .header(
-                "Authorization",
-                format!(
-                    "Bearer {}",
-                    config
-                        .read()
-                        .await
-                        .credentials
-                        .as_ref()
-                        .expect("OAuth credentials are set")
-                        .access_token
-                )
-                .as_str(),
+    // TODO: static client somewhere
+    let client = reqwest::ClientBuilder::new().build()?;
+    let response = client
+        .get(url)
+        .header("Accept", "application/json")
+        .header("User-Agent", user_agent(&config.read().await.client_id))
+        .header(
+            "Authorization",
+            format!(
+                "Bearer {}",
+                config
+                    .read()
+                    .await
+                    .credentials
+                    .as_ref()
+                    .expect("OAuth credentials are set")
+                    .access_token
             )
-            .send()
-            .await;
+            .as_str(),
+        )
+        .send()
+        .await;
 
-        let mut response = match response {
-            Err(e) => {
-                error!("Error when fetching change_id {}: {:?}", change_id, e);
-                error_span!("handle_fetch_error").in_scope(|| {
-                    error!("Error response: {:?}", e);
-                    error!(fetch_error = ?e);
-                    schedule_job(jobs, tx, change_id, config);
-                });
-                return Ok(());
-            }
-            Ok(data) => data,
-        };
-
-        let mut bytes = BytesMut::new();
-        let mut prefetch_done = false;
-        while let Some(chunk) = response.chunk().await? {
-            bytes.extend_from_slice(&chunk);
-
-            if bytes.len() > 120 && !prefetch_done {
-                let next_change_id = parse_change_id_from_bytes(&bytes).unwrap();
-                tracing::trace!(next_change_id = ?next_change_id);
-                prefetch_done = true;
-                schedule_job(jobs.clone(), tx.clone(), next_change_id, config.clone());
-            }
-        }
-
-        // TODO: handle errors by rescheduling based on error
-        let response = serde_json::from_slice::<StashTabResponse>(&bytes)?;
-        debug!(
-            "Read response {} with {} stashes",
-            response.next_change_id,
-            response.stashes.len()
-        );
-        tracing::trace!(number_stashes = ?response.stashes.len());
-
-        // reschedule if payload is empty
-        if response.stashes.is_empty() {
-            info!("Empty response, rescheduling {change_id}");
-            tokio::time::sleep(Duration::from_secs(2)).await;
-            schedule_job(jobs, tx, change_id, config);
+    let mut response = match response {
+        Err(e) => {
+            error!("Error when fetching change_id {}: {:?}", change_id, e);
+            error_span!("handle_fetch_error").in_scope(|| {
+                error!("Error response: {:?}", e);
+                error!(fetch_error = ?e);
+                schedule_job(tx, change_id, config);
+            });
             return Ok(());
         }
+        Ok(data) => data,
+    };
 
-        tx.try_send(IndexerMessage::Tick {
-            response,
-            previous_change_id: change_id.clone(),
-            change_id,
-            created_at: std::time::SystemTime::now(),
-        })
-        .expect("Sending IndexerMessage failed");
+    if response.status() != StatusCode::OK {
+        info!(
+            "Rescheduling in 60s due to HTTP response status code {}",
+            response.status().as_u16()
+        );
+        tokio::time::sleep(Duration::from_secs(60)).await;
+        schedule_job(tx, change_id, config);
+        return Ok(());
     }
+
+    let mut bytes = BytesMut::new();
+    let mut prefetch_done = false;
+    while let Some(chunk) = response.chunk().await? {
+        bytes.extend_from_slice(&chunk);
+
+        if bytes.len() > 120 && !prefetch_done {
+            let next_change_id = parse_change_id_from_bytes(&bytes).unwrap();
+            tracing::trace!(next_change_id = ?next_change_id);
+            prefetch_done = true;
+            schedule_job(tx.clone(), next_change_id, config.clone());
+        }
+    }
+
+    let deserialised = match serde_json::from_slice::<StashTabResponse>(&bytes) {
+        Ok(deserialised) => deserialised,
+        Err(e) => {
+            info!(
+                "Rescheduling in 5s due to deserialization issue {}",
+                e.to_string()
+            );
+            tokio::time::sleep(Duration::from_secs(5)).await;
+            schedule_job(tx, change_id, config);
+            return Ok(());
+        }
+    };
+    debug!(
+        "Read response {} with {} stashes",
+        deserialised.next_change_id,
+        deserialised.stashes.len()
+    );
+    tracing::trace!(number_stashes = ?deserialised.stashes.len());
+
+    // reschedule if payload is empty
+    if deserialised.stashes.is_empty() {
+        info!("Empty response, rescheduling {change_id} in 2s");
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        schedule_job(tx, change_id, config);
+        return Ok(());
+    }
+
+    tx.try_send(IndexerMessage::Tick {
+        response: deserialised,
+        previous_change_id: change_id.clone(),
+        change_id,
+        created_at: std::time::SystemTime::now(),
+    })
+    .expect("Sending IndexerMessage failed");
 
     Ok(())
 }
