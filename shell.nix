927dfc305415ddf2f91d4377dd6052068d5c8a5a
Maximilian Stock <mxmlnstock@googlemail.com>
2024-12-28 22:22:43 +0100

    fix(nix-shell): make shell.nix work on macOS and Linux

--- shell.nix
+++ shell.nix
@@ -1,41 +1,36 @@
 let
-  rustOverlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  rustOverlay = builtins.fetchTarball
+    "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
   pkgs = import <nixpkgs> { overlays = [ (import rustOverlay) ]; };
-in
-pkgs.mkShell rec {
-  buildInputs =
-    with pkgs;
+in pkgs.mkShell rec {
+  buildInputs = with pkgs;
     [
       nixfmt-classic
-      mold
-      clang
       pkg-config
       gettext
       age
-      apacheHttpd
       vegeta
-
-      openssl
-      postgresql
-      (rust-bin.stable.latest.default.override {
-        extensions = [
-          "rust-src"
-          "clippy"
-        ];
-      })
       protobuf
-      sqlx-cli
+      docker-compose
+
+      (rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "clippy" ];
+      })
 
       rust-analyzer
       cargo-edit
       cargo-feature
       cargo-udeps
       cargo-bloat
-      docker-compose
-    ]
-    ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [ linuxPackages.perf ]);
+    ] ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [
+      clang
+      linuxPackages.perf
+      mold
+    ]);
 
   RUST_BACKTRACE = 1;
+
+} // pkgs.lib.mkIf (pkgs.stdenv.isLinux) rec {
   MOLD_PATH = "${pkgs.mold.out}/bin/mold";
   RUSTFLAGS = "-Clink-arg=-fuse-ld=${MOLD_PATH} -Clinker=clang";
   # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
