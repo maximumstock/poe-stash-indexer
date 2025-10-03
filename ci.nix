0c6c803319b6056f345d9907bc12c282e6aa1299
Maximilian Stock <mxmlnstock@googlemail.com>
2025-10-03 15:15:55 +0200

    chore(ci): use crane to build Nix package for indexer

--- ci.nix
+++ ci.nix
@@ -1,43 +1,44 @@
 { pkgs }:
-pkgs.mkShell rec {
+pkgs.mkShell
+  rec {
 
-  buildInputs =
-    with pkgs;
-    [
-      clang
-      nixfmt-classic
-      pkg-config
-      gettext
-      age
-      vegeta
-      docker-compose
+    buildInputs =
+      with pkgs;
+      [
+        clang
+        nixfmt-classic
+        pkg-config
+        gettext
+        age
+        vegeta
+        docker-compose
 
-      (rust-bin.stable.latest.default.override {
-        extensions = [
-          "rust-src"
-          "clippy"
-        ];
-      })
+        (rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "clippy"
+          ];
+        })
 
-      cargo-edit
-      cargo-feature
-      cargo-udeps
-      cargo-bloat
-    ]
-    ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [
-      linuxPackages.perf
-      mold
-    ])
-    ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-      # Additional darwin specific inputs can be set here
-      pkgs.libiconv
-      pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-    ]);
+        cargo-edit
+        cargo-feature
+        cargo-udeps
+        cargo-bloat
+      ]
+      ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [
+        linuxPackages.perf
+        mold
+      ])
+      ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        # Additional darwin specific inputs can be set here
+        pkgs.libiconv
+        pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+      ]);
 
-  RUST_BACKTRACE = 1;
+    RUST_BACKTRACE = 1;
 
-}
-// pkgs.lib.mkIf (pkgs.stdenv.isLinux) rec {
+  }
+  // pkgs.lib.mkIf (pkgs.stdenv.isLinux) rec {
   MOLD_PATH = "${pkgs.mold.out}/bin/mold";
   RUSTFLAGS = "-Clink-arg=-fuse-ld=${MOLD_PATH} -Clinker=clang";
   # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
--- flake.nix
+++ flake.nix
@@ -7,26 +7,33 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      rust-overlay,
+    { self
+    , nixpkgs
+    , flake-utils
+    , rust-overlay
+    , crane
+    ,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-
+        craneLib = crane.mkLib pkgs;
       in
       {
         devShell = import ./ci.nix { inherit pkgs; };
         formatter = pkgs.nixpkgs-fmt;
+        packages.indexer = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          pname = "indexer";
+          cargoExtraArgs = "-p indexer";
+          cargoClippyExtraArgs = "--all-features --all-targets -- -D warnings";
+        };
       }
-
     );
 }
